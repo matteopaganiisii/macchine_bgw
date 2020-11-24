d8c20d79bdc4f1e8a202c8339dd41704edf82db5
matteopaganiisii <74557950+matteopaganiisii@users.noreply.github.com>
2020-11-24 13:04:05 +0100

    addedd programm

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Esercizio2_BGW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.pic_red = new System.Windows.Forms.PictureBox();
+            this.pic_black = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_black)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(78, 234);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(274, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // pic_red
+            // 
+            this.pic_red.Image = global::Esercizio2_BGW.Properties.Resources.macchinarossa;
+            this.pic_red.Location = new System.Drawing.Point(8, 142);
+            this.pic_red.Name = "pic_red";
+            this.pic_red.Size = new System.Drawing.Size(100, 50);
+            this.pic_red.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_red.TabIndex = 3;
+            this.pic_red.TabStop = false;
+            // 
+            // pic_black
+            // 
+            this.pic_black.Image = global::Esercizio2_BGW.Properties.Resources.macchinabianca;
+            this.pic_black.Location = new System.Drawing.Point(8, 40);
+            this.pic_black.Name = "pic_black";
+            this.pic_black.Size = new System.Drawing.Size(100, 50);
+            this.pic_black.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_black.TabIndex = 2;
+            this.pic_black.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Esercizio2_BGW.Properties.Resources.road_trip_background_10864211;
+            this.pictureBox1.Location = new System.Drawing.Point(-69, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(520, 210);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 269);
+            this.Controls.Add(this.pic_red);
+            this.Controls.Add(this.pic_black);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Macchine";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_black)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pic_black;
+        private System.Windows.Forms.PictureBox pic_red;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esercizio2_BGW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        BackgroundWorker bgw, bgw1;
+
+        private void bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //dichiarazione variabili
+            BackgroundWorker worker = sender as BackgroundWorker;
+            Random rnd = new Random();
+            string[] info = new string[2];
+            int somma = 0;
+            bool check = true;
+
+            //assegnazione informazioni da trasmettere - colore e movimento
+            info[0] = (string)e.Argument;
+
+            while (somma <= 350 && check == true)
+            {
+                Thread.Sleep(30); // aspetta 30 ms
+
+                //random velocità
+                info[1] = rnd.Next(0, 5).ToString();
+                somma += int.Parse(info[1]);
+
+                //invio values
+                worker.ReportProgress(0, info);
+                e.Result = info[0];
+
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    check = false;
+                }
+            }
+        }
+
+        private void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //ottenimento dati dal bgw
+            string[] info = (string[])e.UserState;
+
+            //perform steps
+            if (info[0] == "black")
+            {
+                pic_black.Left += int.Parse(info[1]);
+            }
+            else if(info[0] == "red")
+            {
+                pic_red.Left += int.Parse(info[1]);
+            }
+        }
+
+        private void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                //annullato
+            }
+            else if ((string)e.Result == "black")
+            {
+                //se vince la nera ferma la rossa
+                bgw1.CancelAsync();
+                MessageBox.Show("winner : black");
+
+            }
+            else if ((string)e.Result == "red")
+            {
+                //se vince la rossa ferma la nera
+                bgw.CancelAsync();
+                MessageBox.Show("winner : red");
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            
+            bgw = new BackgroundWorker();
+            bgw.WorkerReportsProgress = true;
+            bgw.WorkerSupportsCancellation = true;
+            bgw.DoWork += bgw_DoWork;
+            bgw.ProgressChanged += bgw_ProgressChanged;
+            bgw.RunWorkerCompleted += bgw_RunWorkerCompleted;
+
+            bgw1 = new BackgroundWorker();
+            bgw1.WorkerReportsProgress = true;
+            bgw1.WorkerSupportsCancellation = true;
+            bgw1.DoWork += bgw_DoWork;
+            bgw1.ProgressChanged += bgw_ProgressChanged;
+            bgw1.RunWorkerCompleted += bgw_RunWorkerCompleted;
+
+            //prima macchina parte
+            bgw.RunWorkerAsync("black");
+
+            //aspetta 100ms per problema ai 2 generatori random
+            Thread.Sleep(100);
+
+            bgw1.RunWorkerAsync("red");
+
+        }
+    }
+}
